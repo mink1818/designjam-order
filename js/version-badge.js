56bc0c07ed02e0962854f10ffff61feeedb122a4
mink1818 <ismk1818@gmail.com>
2026-07-19 16:38:29 +0900

    release v5.3.12 mobile brand favorite position

--- js/version-badge.js
+++ js/version-badge.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='V5.3.5';
+  const VERSION='V5.3.12';
   function addBadge(){
     if(document.getElementById('appVersionBadge')) return;
     const b=document.createElement('div');
